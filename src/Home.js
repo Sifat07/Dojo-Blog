38e5183416fcc74d089b820a201e3462a3386175
Sifat07 <sifat140693@gmail.com>
2021-09-07 18:05:28 +0600

    third commit

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,15 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
-  const [isPending, setIsPending]= useState(true);
-  const [error, setError] = useState(null);
-
-
-  useEffect(() => {
-    setTimeout( () => {
-      fetch('http://localhost:8000/blogs')
-    .then (res => {
-      if(!res.ok) {
-        throw Error('Could not fetch data from that resource');
-      }
-      return res.json();
-    })
-    .then ( data => {
-      setBlogs(data);
-      setIsPending(false);
-      setError(null);
-    })
-    .catch(err => {
-      setIsPending(false);
-      setError(err.message);
-    })
-    },1000);
-  }, []);
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
   return (
     <div className="home">
-      {error && <div>{error}</div> }
-      {isPending && <div> Loading. . . </div> }
-      { blogs && <BlogList blogs={blogs} title="All Blogs" />}
-      {/* <button onClick={() => setName('luigi')}>change name</button> */}
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
